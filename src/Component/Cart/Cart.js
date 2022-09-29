5cbc6b4c0969507264e325f7f4a9397e893a59d8
sanjitkumer <sanjitk666@gmail.com>
2022-09-29 15:08:31 +0600

    more added cart

--- src/Component/Cart/Cart.js
+++ src/Component/Cart/Cart.js
@@ -1,29 +1,64 @@
+import { faLocation } from '@fortawesome/free-solid-svg-icons';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import React from 'react';
 import './Cart.css';
 
 const Cart = ({cart}) => {
     return (
         <div className='cart'>
-         <div className='part-1'>
-            <h3>Sanjit Kumer</h3>
-            <p>Dhaka,Bangladesh</p> 
-          </div> 
-          <div className='part-2'>
-              <div>
-              <h2>75 <small>kg</small></h2>
-              <h3>weight</h3>
-              </div>
+            <div className='part-1'>
+                <h1>Sanjit Kumer</h1>
+                <div className='location'>
+                 <FontAwesomeIcon icon = {faLocation} ></FontAwesomeIcon>
+                 <p >Dhaka,Bangladesh</p> 
 
-              <div>
-              <h2>75 <small>kg</small></h2>
-              <h3>weight</h3>
-              </div>
+                </div>
+                
+            </div> 
 
-              <div>
-              <h2>75 <small>kg</small></h2>
-              <h3>weight</h3>
-              </div>
-          </div>            
+            <div className='part-2'>
+                <div>
+                <h2>75 <small>kg</small></h2>
+                <h3>weight</h3>
+                </div>
+
+                <div>
+                <h2>6.5</h2>
+                <h3>Height</h3>
+                </div>
+
+                <div>
+                <h2>25 <small>yrs</small></h2>
+                <h3>Age</h3>
+                </div>
+            </div> 
+
+            <div className='part-3'>
+                <h2> Add A Break</h2>                
+            </div>
+            <div className='part-4'>
+                <button>10 m</button>
+                <button>15 m</button>
+                <button>30 m</button>
+                <button>20 m</button>          
+            </div>
+
+            <div className='part-5'>
+                <h2>Exercise Details</h2>
+
+                <div className='exercise-time'>
+                    <h4>Exercise Time :</h4>
+                </div>
+                <div className=''>
+                    <h4>Break time : </h4>
+                </div>
+            </div>
+
+            <button>
+                <h3>Activity Complete</h3>
+            </button>
+
+
             
         </div>
     );
