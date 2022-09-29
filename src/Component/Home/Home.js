1eee19ddd1bb8802001f78cce90e42e36b332fa7
sanjitkumer <sanjitk666@gmail.com>
2022-09-29 08:12:55 +0600

    added home part

---
+++ src/Component/Home/Home.js
@@ -0,0 +1,20 @@
+import React from 'react';
+
+const Home = () => {
+    return (
+        <div className='home-container'>
+            <div className="main-container">
+                <h2> This is main section.</h2>
+                <p></p>             
+            
+
+            </div>
+            <div className="cart-container">
+                <h3>Profile</h3>
+                <p></p>
+            </div>
+        </div>
+    );
+};
+
+export default Home;
